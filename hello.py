41f4819de996524956cc61731d3b17df495197a0
felipe bastos <felipebastosweb@gmail.com>
2017-07-02 21:42:33 -0300

    Create hello.py

---
+++ hello.py
@@ -0,0 +1,13 @@
+import os
+from weppy import App
+
+app = App(__name__)
+
+@app.route('/')
+def welcome_index():
+    return 'Hello World'
+
+if __name__ == "__main__":
+    # important !!!!!!! 5000 or PORT env of Heroku
+    port = os.getenv('PORT', 5000)
+    app.run(host='0.0.0.0', port=int(port), debug=True)
