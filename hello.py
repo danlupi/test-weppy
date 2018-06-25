4236370b4de8eb9915c3b0a1e960e76d5f3d866b
Danny <danny.lupinelli@hulu.com>
2018-06-25 03:44:49 -0700

    some new commit

--- hello.py
+++ hello.py
@@ -8,6 +8,7 @@ def welcome_index():
     return 'Hello World'
 
 if __name__ == "__main__":
+    # tet htis out
     # important !!!!!!! 5000 or PORT env of Heroku
     port = os.getenv('PORT', 5000)
     app.run(host='0.0.0.0', port=int(port), debug=True)
